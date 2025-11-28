84d88ba55e5213d3a4caabf39ca5db805b797095
Anuji Nishala Weragoda <anuji.w@eyepax.com>
2025-11-28 21:02:03 +0530

    websocket in kotlin app

--- app/src/main/java/com/example/androidapp/data/remote/WebSocketService.kt
+++ app/src/main/java/com/example/androidapp/data/remote/WebSocketService.kt
@@ -3,14 +3,17 @@ package com.example.androidapp.data.remote
 import okhttp3.*
 
 class WebSocketService(
-    private val url: String
+    private val url: String = "wss://ws.postman-echo.com/raw"
+
 ) {
 
     private var webSocket: WebSocket? = null
+    private val client = OkHttpClient()
 
     fun connect(listener: WebSocketListener) {
-        val client = OkHttpClient()
-        val request = Request.Builder().url(url).build()
+        val request = Request.Builder()
+            .url(url)
+            .build()
 
         webSocket = client.newWebSocket(request, listener)
     }
